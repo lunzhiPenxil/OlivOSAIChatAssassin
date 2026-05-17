fa2c662e3a434cc4fdc7da8ac120994c50191b24
仑质 <lunzhipenxil@gmail.com>
2026-05-17 13:58:43 +0800

    改进日志调用

--- OlivOSAIChatAssassin/logger.py
+++ OlivOSAIChatAssassin/logger.py
@@ -1,6 +1,13 @@
 import OlivOSAIChatAssassin
 
 
+# 日志级别常量
+LOG_DEBUG = 0
+LOG_INFO = 2
+LOG_WARN = 3
+LOG_ERROR = 4
+
+
 def logRaw(level: int, msg: str):
     if OlivOSAIChatAssassin.data.gProc is not None:
         OlivOSAIChatAssassin.data.gProc.log(level, msg, [
@@ -9,8 +16,8 @@ def logRaw(level: int, msg: str):
 
 
 def log(msg: str):
-    logRaw(2, msg)
+    logRaw(LOG_INFO, msg)
 
 
 def warn(msg: str):
-    logRaw(3, msg)
+    logRaw(LOG_WARN, msg)
